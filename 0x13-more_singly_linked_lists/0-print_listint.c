a3da3e7bf803ce8ebd3775f208e0476b3af29ab2
Tiger Blessing <Blessingtiger2020@gmail.com>
2023-10-24 14:39:31 -0700

    blessing

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t num = 0;
+size_t list = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-return (num);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+list++;
+h = h->next;
+}
+return (list);
 }
