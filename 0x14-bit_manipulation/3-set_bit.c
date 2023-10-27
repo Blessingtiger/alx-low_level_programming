d643c1aab43328500af637e2e052cdd7124f7461
Tiger Blessing <Blessingtiger2020@gmail.com>
2023-10-26 17:08:22 -0700

    bless

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,16 +5,16 @@
  * @n: number to set
  * @index: value of a bit to 1 at a given index
  *
- * return: 1 if it worked, or -1
+ * Return: 1 if it worked, or -1
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int bit;
+unsigned long int setbit;
 
 if (index > (sizeof(unsigned long int) * 8 - 1))
 return (-1);
-bit = 1 << index;
-*n = *n | bit;
+setbit = 1 << index;
+*n = *n | setbit;
 return (1);
 }
 
